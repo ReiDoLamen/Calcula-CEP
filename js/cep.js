7774c659790bb2eacf10602b81a81653e24336fb
unisanta <unisanta@santacecilia.br>
2023-11-08 22:15:48 -0300

    aula sobrino

--- js/cep.js
+++ js/cep.js
@@ -1,6 +1,7 @@
 const cep = document.getElementById("cep");
 const btnPesquisar = document.getElementById("btnPesquisar");
 const saida = document.getElementById("saida");
+const endereco = document.getElementById("migo");
 
 // document.getElementById("saida").textContent= "Oi, mundo"
 
@@ -9,13 +10,29 @@ function obterCEP() {
 
 }
 
-async function mostrarNaTela () {
+ function apresentarDadosCEP(obj) {
+    return(!obj.erro)?
+    `${obj.logradouro} 
+    ${obj.complemento} 
+    ${obj.bairro} 
+    ${obj.localidade}` : `CEP NÃO ENCONTRADO`
+
+}
+ 
+
+
+async function buscarDadosCEP () {
+  try{
     let urlCEP = `https://viacep.com.br/ws/${obterCEP()}/json/`;
     let trazerCep = fetch(urlCEP);
     let resposta = await trazerCep;
     let dadosJson = await resposta.json();
+    saida.innerText = apresentarDadosCEP(dadosJson);
 
-    console.log(dadosJson)
+  } catch(e) {
+      saida.innerHTML = `<b>error</b>: <i>${e}</i>`;
+  }
+
 }
 
-btnPesquisar.addEventListener("click", mostrarNaTela);
+btnPesquisar.addEventListener("click", buscarDadosCEP);
